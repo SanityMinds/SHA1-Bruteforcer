6c8720399e275acd44e7e0fbd17ae179108bf7dd
SanityMinds <138660341+SanityMinds@users.noreply.github.com>
2024-05-07 06:05:05 +0100

    Update brute.py

--- brute.py
+++ brute.py
@@ -9,7 +9,6 @@ def sha1(text):
     return s.hexdigest()
 
 def get_charset(mode):
-    # Returning different character sets based on the mode
     if mode == "Alphabetical":
         return string.ascii_letters
     elif mode == "Numbers":
@@ -23,14 +22,13 @@ def get_charset(mode):
     elif mode == "Only Uppercase":
         return string.ascii_uppercase
     elif mode == "All":
-        return string.printable[:-6]  # Excludes the last 6 non-printable characters from string.printable
+        return string.printable[:-6]
     else:
         return string.printable[:-6]
 
 def brute_force_sha1(target_hash, min_length, max_length, mode):
     charset = get_charset(mode)
     for length in range(min_length, max_length + 1):
-        print(f"Trying length {length}")
         for guess in itertools.product(charset, repeat=length):
             guess_text = "".join(guess)
             if sha1(guess_text) == target_hash:
@@ -38,39 +36,26 @@ def brute_force_sha1(target_hash, min_length, max_length, mode):
                 return guess_text
     return None
 
-    if __name__ == "__main__":
-    # starting screen 
-        print("Made By Bytelabs")
+if __name__ == "__main__":
+    print("Made By Bytelabs")
+    print("""
+d8888b. db    db d888888b d88888b db       .d8b.  d8888b. .d8888.
+88  `8D `8b  d8' `~~88~~' 88'     88      d8' `8b 88  `8D 88'  YP
+88oooY'  `8bd8'     88    88ooooo 88      88ooo88 88oooY' `8bo.
+88~~~b.    88       88    88~~~~~ 88      88~~~88 88~~~b.   `Y8b.
+88   8D    88       88    88.     88booo. 88   88 88   8D db   8D
+Y8888P'    YP       YP    Y88888P Y88888P YP   YP Y8888P' `8888Y'
+    """)
+    print("Starting the script....")
+    time.sleep(5)
     
-        
-        print("\033[94m")
-        print("""
-    d8888b. db    db d888888b d88888b db       .d8b.  d8888b. .d8888.
-    88  `8D `8b  d8' `~~88~~' 88'     88      d8' `8b 88  `8D 88'  YP
-    88oooY'  `8bd8'     88    88ooooo 88      88ooo88 88oooY' `8bo.
-    88~~~b.    88       88    88~~~~~ 88      88~~~88 88~~~b.   `Y8b.
-    88   8D    88       88    88.     88booo. 88   88 88   8D db   8D
-    Y8888P'    YP       YP    Y88888P Y88888P YP   YP Y8888P' `8888Y
-        """)
-        print("\033[0m")
-    
-        
-        print("Starting the script....")
-        time.sleep(5)
-        
-        # Target hash to brute-force
-        target_hash = "5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8"  # Example SHA-1 hash for the password "password" This MUST be a SHA1 hash.
-
-    # Choose the mode for the character set
-    mode = "all"  # Options: "Alphabetical", "Numbers", "Basic Symbols", "All Symbols", "Only Lowercase", "Only Uppercase", "All"
-    
-    # I recommend lowercase as it is faster 
-
-    # Warning: This can take a very long time depending on the min_length, max_length, and charset
-    min_length = 1  # Minimum length of the string to be guessed
-    max_length = 5 # Maximum length of the string to be guessed. 
-
+    target_hash = "5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8" # "password"
+    mode = "Only Lowercase"
+    min_length = 1
+    max_length = 5
     found_text = brute_force_sha1(target_hash, min_length, max_length, mode)
 
     if not found_text:
         print("Not found within the given length and charset.")
+
+    input("\nPress Enter to exit...") 
